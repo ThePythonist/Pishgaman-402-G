2dd4387696ec65b93e2604de8accf89aa09f6421
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-10-28 19:28:48 +0330

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,27 @@
+# import random
+#
+# x = int(input("Start : "))
+# y = int(input("End : "))
+#
+# print(random.randint(x,y))
+
+# ---------------------------------
+
+# import random
+#
+# students = ["meysam", "amirali", "sepehr", "aria"]
+# print(random.choice(students))
+
+# ---------------------------------
+
+# import random
+#
+# numbers = [15, 10, 20, 5, 35, 40]
+# print(random.choice(numbers))
+
+# ---------------------------------
+
+import random
+
+numbers = [15, 10, 20, 5, 35, 40]
+print(random.sample(numbers, 2))
